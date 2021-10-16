9e767acca48e87b0797d7defbf61ac51b2614888
Kevin Hobbs <kevinhobbs@gmail.com>
2021-10-15 21:54:52 -0500

    More tweaks

--- dialogic/RandomSpeechBubble.gd
+++ dialogic/RandomSpeechBubble.gd
@@ -3,6 +3,8 @@ extends MarginContainer
 var texture_size:Vector2
 var texture_rect:Vector2
 var valid_range:Vector2
+export(bool) var allow_flip_h = true
+export(bool) var allow_flip_v = true
 
 func _ready():
 	texture_size = $TextureOverlay.texture.get_size()
@@ -12,8 +14,8 @@ func _ready():
 func move_paper():
 	$TextureOverlay.region_rect.position.x = int(rand_range(0, valid_range.x))
 	$TextureOverlay.region_rect.position.y = int(rand_range(0, valid_range.y))
-	$TextureOverlay.flip_h = randf() >= 0.5;
-	$TextureOverlay.flip_v = randf() >= 0.5;
+	if allow_flip_h: $TextureOverlay.flip_h = randf() >= 0.5;
+	if allow_flip_v: $TextureOverlay.flip_v = randf() >= 0.5;
 
 func _on_Timer_timeout():
 	move_paper()

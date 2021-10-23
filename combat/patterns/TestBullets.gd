43adab9a071641896ee2712f21ab62907d8a1c1c
Kevin Hobbs <kevinhobbs@gmail.com>
2021-10-22 19:10:39 -0500

    Support saving bullet patterns

--- combat/patterns/TestBullets.gd
+++ combat/patterns/TestBullets.gd
@@ -1,6 +1,7 @@
+tool
 extends Node2D
 
-
+export(bool) var save_pattern = false setget _on_SaveButton_pressed
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -20,3 +21,19 @@ func _ready():
 func _on_BulletPattern_attack_complete():
 	yield(get_tree().create_timer(3), "timeout")
 	$BulletPattern._ready()
+
+
+func _on_SaveButton_pressed(val):
+	var scene = preload("res://combat/patterns/BulletPattern.tscn").instance()
+	scene.num_bullets = $BulletPattern.num_bullets
+	scene.attack_time = $BulletPattern.attack_time
+	scene.min_speed = $BulletPattern.min_speed
+	scene.max_speed = $BulletPattern.max_speed
+	scene.bullet_timing = $BulletPattern.bullet_timing.duplicate()
+	scene.bullet_target = $BulletPattern.bullet_target.duplicate()
+	scene.bullet_speed = $BulletPattern.bullet_speed.duplicate()
+	scene.bullet_origin_scene = $BulletPattern.bullet_origin_scene
+	scene.bullet_scene = $BulletPattern.bullet_scene
+	var packed_scene = PackedScene.new()
+	packed_scene.pack(scene)
+	ResourceSaver.save("res://combat/patterns/_NEW_BULLET_PATTERN.tscn", packed_scene)

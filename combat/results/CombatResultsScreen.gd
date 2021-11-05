e56a31a08699664552063f8e0625e85ac02279ea
Kevin Hobbs <kevinhobbs@gmail.com>
2021-11-05 17:02:13 -0500

    Fix broken animations

--- combat/results/CombatResultsScreen.gd
+++ combat/results/CombatResultsScreen.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	for child in get_children():
+	$Background.playing = true
+	for child in $StatsRows.get_children():
 		if child is AnimatedSprite:
 			child.playing = true
 	find_node("AnimationPlayer").play("rank_gleam")
